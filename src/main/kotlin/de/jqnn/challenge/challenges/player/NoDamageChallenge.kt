6d94ae5ef3a0d74062bcf47d9fc88d1f9d4e5074
jqnn. <scherping.jan@icloud.com>
2024-12-27 01:58:05 +0100

    Update NoDamageChallenge.kt
    add death event for global death and join for new players

--- src/main/kotlin/de/jqnn/challenge/challenges/player/NoDamageChallenge.kt
+++ src/main/kotlin/de/jqnn/challenge/challenges/player/NoDamageChallenge.kt
@@ -1,10 +1,29 @@
 package de.jqnn.challenge.challenges.player
 
 import de.jqnn.challenge.challenges.Challenge
+import de.jqnn.challenge.timer.TimerState
+import net.axay.kspigot.event.listen
 import org.bukkit.Bukkit
 import org.bukkit.Material
+import org.bukkit.event.entity.PlayerDeathEvent
+import org.bukkit.event.player.PlayerJoinEvent
 
 class NoDamageChallenge : Challenge("0.5 Herzen", Material.APPLE, emptyList(), "§c") {
+
+    init {
+        listen<PlayerDeathEvent> {
+            if (!this.timer.isRunning()) return@listen
+            if (!(this.enabled)) return@listen
+            it.deathMessage(null)
+            timer.updateState(TimerState.CHALLENGE_LOOSE, "§c" + it.entity.name + " §rhat §cSchaden §rerlitten§8.")
+        }
+
+        listen<PlayerJoinEvent> {
+            if (!(this.enabled)) return@listen
+            it.player.maxHealth = 0.5
+            it.player.health = 0.5
+        }
+    }
 
     override fun onEnable() {
         Bukkit.getOnlinePlayers().forEach {
